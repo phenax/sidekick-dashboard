b87cdc733cf7ed1c954340e771b12243cddc13b6
Akshay Nair <phenax5@gmail.com>
2023-03-07 18:03:26 +0530

    chore: fixes test case to use random id

--- src/components/Tasks/_update.spec.ts
+++ src/components/Tasks/_update.spec.ts
@@ -2,6 +2,15 @@ import { runEffect } from '../../utils/solid'
 import { Action, State, UI } from './types'
 import { update } from './update'
 
+vi.mock('../../utils/helpers.ts', async () => {
+  const actual: any = await vi.importActual('../../utils/helpers.ts')
+
+  return {
+    ...actual,
+    uuid: () => 'r@nd0m',
+  }
+})
+
 describe('update', () => {
   it('should perform basic task list actions', () => {
     const state: State = {
@@ -34,9 +43,10 @@ describe('update', () => {
     })
     expect(nextState(Action.AddTask())).toEqual({
       ...state,
-      highlightedIndex: 3,
+      highlightedIndex: 2,
       editing: true,
-      tasks: {...state.tasks, '6': { id: '6', text: '' }},
+      tasks: {...state.tasks, 'r@nd0m': { id: 'r@nd0m', text: '' }},
+      taskOrder: [ '1', '2', 'r@nd0m', '3' ],
     })
     expect(
       nextState(Action.SetContents({ id: '2', value: 'Foobar' }))
@@ -52,7 +62,16 @@ describe('update', () => {
       chainActions([Action.ToggleCheck('2'), Action.ToggleCheck('2')])
     ).toEqual({
       ...state,
-      tasks: { ...state.tasks, '2': { id: '2', text: 'Buy milk', checked: false }},
+      tasks: { ...state.tasks, '2': { id: '2', text: 'Pour milk on self', checked: false }},
+    })
+
+
+    expect(nextState(Action.AddTask(), { ...state, tasks: {}, taskOrder: [] })).toEqual({
+      ...state,
+      highlightedIndex: 2,
+      editing: true,
+      tasks: {'r@nd0m': { id: 'r@nd0m', text: '' }},
+      taskOrder: [ 'r@nd0m' ],
     })
   })
 })
